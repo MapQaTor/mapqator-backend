f35850f789e296b6e7aeaa55af18e7633fe7a556
Mahir Labib Dihan <mahirlabibdihan@gmail.com>
2024-04-29 12:40:53 +0600

    fix

--- controllers/gptController.js
+++ controllers/gptController.js
@@ -2,69 +2,77 @@ const { OpenAIClient, AzureKeyCredential } = require("@azure/openai");
 const queryRepository = require("../repositories/queryRepository");
 
 const client = new OpenAIClient(
-  "https://qcri-llm-rag-5.openai.azure.com/",
-  new AzureKeyCredential(process.env.AZURE_OPENAI_KEY)
+	"https://qcri-llm-rag-5.openai.azure.com/",
+	new AzureKeyCredential(process.env.AZURE_OPENAI_KEY)
 );
 
 const generateContext = async (req, res) => {
-  console.log(req.body);
+	console.log(req.body);
 };
 
 const askGPT = async (req, res) => {
-  console.log("Hit");
-  const result = await queryRepository.getQuery(req.params.id);
-  if (result.success) {
-    const { question, context_gpt, answer } = result.data[0];
+	console.log("Hit");
+	const result = await queryRepository.getQuery(req.params.id);
+	if (result.success) {
+		const { question, context_gpt, answer } = result.data[0];
 
-    // Convert answer.options to string
-    const options = answer.options
-      .map((option, index) => "Option " + (index + 1) + ". " + option.text)
-      .join("\n");
-    const message_text = [
-      {
-        role: "system",
-        content: "You are an AI assistant that helps people find information.",
-      },
-      {
-        role: "user",
-        content: `I will ask you MCQ questions. You just need to answer numerically (e.g., 1/2/...). No explanation needed.only a number for example if its option 3 just say 3`,
-      },
-      {
-        role: "user",
-        content: `Context:\n${context_gpt} Question:\n${question} Options:\n${options}`,
-      },
-    ];
-    try {
-      const { choices } = await client.getChatCompletions(
-        "GPT-35-TURBO-0125",
-        question,
-        {
-          max_tokens: 800,
-          temperature: 0,
-          top_p: 1,
-          frequency_penalty: 0,
-          presence_penalty: 0,
-          // stop: ["\n"],
-          messages: message_text,
-        }
-      );
-      // check if choices[0].message["content"] is a number
-      // if not, return the message
-      // if it is a number, check if it is within the range of the options
-      // if not, return the message
-      // if it is within the range of the options, return the message
+		// Convert answer.options to string
+		const options = answer.options
+			.map((option, index) => "Option " + (index + 1) + ". " + option)
+			.join("\n");
+		const message_text = [
+			{
+				role: "system",
+				content:
+					"You are an AI assistant that helps people find information.",
+			},
+			{
+				role: "user",
+				content: `I will ask you MCQ questions. You just need to answer numerically (e.g., 1/2/...). No explanation needed.only a number for example if its option 3 just say 3`,
+			},
+			{
+				role: "user",
+				content: `Context:\n${context_gpt} Question:\n${question} Options:\n${options}`,
+			},
+		];
 
-      console.log(choices[0].message["content"][0]);
-      res.send(choices[0].message["content"][0]);
-    } catch (error) {
-      console.error("An error occurred:", error);
-      res.status(500).send({ success: false, error: "An error occurred" });
-    }
-  } else {
-    res.status(404).send(result);
-  }
+		console.log(
+			`Context:\n${context_gpt} Question:\n${question} Options:\n${options}`
+		);
+		try {
+			const { choices } = await client.getChatCompletions(
+				"GPT-35-TURBO-0125",
+				question,
+				{
+					max_tokens: 800,
+					temperature: 0,
+					top_p: 1,
+					frequency_penalty: 0,
+					presence_penalty: 0,
+					// stop: ["\n"],
+					messages: message_text,
+				}
+			);
+			// check if choices[0].message["content"] is a number
+			// if not, return the message
+			// if it is a number, check if it is within the range of the options
+			// if not, return the message
+			// if it is within the range of the options, return the message
+
+			console.log(choices[0].message["content"][0]);
+			res.send(choices[0].message["content"][0]);
+		} catch (error) {
+			console.error("An error occurred:", error);
+			res.status(500).send({
+				success: false,
+				error: "An error occurred",
+			});
+		}
+	} else {
+		res.status(404).send(result);
+	}
 };
 module.exports = {
-  generateContext,
-  askGPT,
+	generateContext,
+	askGPT,
 };
--- repositories/queryRepository.js
+++ repositories/queryRepository.js
@@ -1,60 +1,60 @@
 const base = require("./base");
 
 const createQuery = async (record) => {
-  const query =
-    "INSERT INTO dataset (question, answer, context, context_json, classification, context_gpt) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *";
-  const params = [
-    record.question,
-    record.answer,
-    record.context,
-    record.context_json,
-    record.classification,
-    record.context_gpt,
-  ];
-  const result = await base.query(query, params);
-  return result;
+	const query =
+		"INSERT INTO dataset (question, answer, context, context_json, classification, context_gpt) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *";
+	const params = [
+		record.question,
+		record.answer,
+		record.context,
+		record.context_json,
+		record.classification,
+		record.context_gpt,
+	];
+	const result = await base.query(query, params);
+	return result;
 };
 
 const getQuery = async (id) => {
-  const query = "SELECT * FROM dataset WHERE id = $1";
-  const params = [id];
-  const result = await base.query(query, params);
-  return result;
+	const query = "SELECT * FROM dataset WHERE id = $1";
+	const params = [id];
+	const result = await base.query(query, params);
+	return result;
 };
 
 const getQueries = async () => {
-  const query = "SELECT * FROM dataset ORDER BY id ASC";
-  const result = await base.query(query);
-  return result;
+	const query = "SELECT * FROM dataset ORDER BY id DESC";
+	const result = await base.query(query);
+	return result;
 };
 
 const updateQuery = async (id, record) => {
-  const query =
-    "UPDATE dataset SET question = $1, answer = $2, context = $3, context_json = $4, classification = $5, context_gpt = $6 WHERE id = $7 RETURNING *";
-  const params = [
-    record.question,
-    record.answer,
-    record.context,
-    record.context_json,
-    record.classification,
-    record.context_gpt,
-    id,
-  ];
-  const result = await base.query(query, params);
-  return result;
+	const query =
+		"UPDATE dataset SET question = $1, answer = $2, context = $3, context_json = $4, classification = $5, context_gpt = $6 WHERE id = $7 RETURNING *";
+	const params = [
+		record.question,
+		record.answer,
+		record.context,
+		record.context_json,
+		record.classification,
+		record.context_gpt,
+		id,
+	];
+	const result = await base.query(query, params);
+	return result;
 };
 
 const deleteQuery = async (id) => {
-  const query = "DELETE FROM dataset WHERE id = $1";
-  const params = [id];
-  const result = await base.query(query, params);
-  return result;
+	const query = "DELETE FROM dataset WHERE id = $1";
+	const params = [id];
+	const result = await base.query(query, params);
+	return result;
 };
 
 module.exports = {
-  createQuery,
-  getQuery,
-  getQueries,
-  updateQuery,
-  deleteQuery,
+	createQuery,
+	getQuery,
+	getQueries,
+	updateQuery,
+	deleteQuery,
 };
